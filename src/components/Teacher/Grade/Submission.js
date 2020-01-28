6370fef0115d870fc49c68af596c421ca4d26ba3
Long H. Nguyen <lohnguyen47@gmail.com>
2020-01-27 20:20:18 -0800

    Add changed file

--- src/components/Teacher/Grade/Submission.js
+++ src/components/Teacher/Grade/Submission.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
 
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
