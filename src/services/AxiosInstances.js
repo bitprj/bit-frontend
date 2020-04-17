bb6b612291e040cb6e1f36bb764459ea06fb32a1
Bryan Zhang <36049897+poroia@users.noreply.github.com>
2020-04-16 17:51:10 -0700

    Update baseUrl to production

--- src/services/AxiosInstances.js
+++ src/services/AxiosInstances.js
@@ -6,8 +6,8 @@ import store from '../redux/store'
 
 /** GENERAL BACKEND (mainly for GET) */
 
-// const baseUrl = 'https://wongband.pythonanywhere.com/'
-export const baseUrl = 'https://bit-backend-staging.herokuapp.com/'
+export const baseUrl = 'https://wongband.pythonanywhere.com/'
+// export const baseUrl = 'https://bit-backend-staging.herokuapp.com/'
 // const baseUrl = 'https://darlene-backend.herokuapp.com/'
 // export const baseUrl = 'https://214509c7.ngrok.io'
 // const baseUrl = 'http://localhost:5000/'
