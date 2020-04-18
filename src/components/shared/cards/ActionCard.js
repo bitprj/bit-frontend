50a0beae77d38012f9ad07cd6fdaad4a7fa3f9c0
Tom Stepp <tstepp@usc.edu>
2020-04-17 20:07:25 -0700

    Updated card component

--- src/components/shared/cards/ActionCard.js
+++ src/components/shared/cards/ActionCard.js
@@ -1,20 +1,46 @@
 import React from 'react';
+import { FaFlagCheckered, } from 'react-icons/fa';
+import { AiTwotonePieChart } from 'react-icons/ai';
+import styled from 'styled-components';
+
+const BlueCard = styled.div`
+  text-align: center;
+  background-color: rgba(44,114,223, 0.85);
+  border-radius: 8px;
+  margin: 1em 1em;
+  padding: 0.25em 1em;
+  max-width: 600px;
+`
+
+const WhiteIcon = styled.div`
+  color: white;
+  margin: 2em 1em 1em 1em;
+  padding: 0.25em 1em;
+`
+
+const CardText = styled.h1`
+  color: white;
+  margin: 1em 1em;
+  padding: 0.25em 1em;
+`
 
 const ActionCard = (props) => {
-    var text;
+    var text, icon, size=48;
     if (props.type === 'resume'){
       text = 'Resume Activity';
-      // TODO: icon = pie chart
+      icon = <AiTwotonePieChart size={size}/>;
     } else if (props.type === 'start') {
       text = 'Start Activity';
-      // TODO: icon = race flag
+      icon = <FaFlagCheckered size={size}/>;
     } else {
-      // TODO: throw error! you should not be here.
+      // Invalid style property, you should not be here.
     }
+
     return (
-        <div>
-          <h1>{text}</h1>
-        </div>
+        <BlueCard>
+          <WhiteIcon>{icon}</WhiteIcon>
+          <CardText>{text}</CardText>
+        </BlueCard>
     )
 }
 
